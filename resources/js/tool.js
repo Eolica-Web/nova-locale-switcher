ec3e3f23f35a0d390548bd7645b6d5d766555adf
anditsung <anditsung@gmail.com>
2022-05-21 13:01:01 +0700

    remove comment on tool.js

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -2,12 +2,4 @@ import NovaThemeDropdown from "./components/NovaThemeDropdown"
 
 Nova.booting((app, store) => {
     app.component('ThemeDropdown', NovaThemeDropdown)
-    // app.component('HelpText', {
-    //     template: `<p class="help-text cursor-pointer" @click="displayWarning"><slot /></p>`,
-    //     methods: {
-    //         displayWarning() {
-    //             window.alert('HelpText alert')
-    //         }
-    //     }
-    // })
 })
